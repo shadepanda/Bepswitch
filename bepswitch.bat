08fdedb1b50aeef4051c51d329822338f3870f00
lauren-aka-proudunicornmods <proudunicorn.mods@gmail.com>
2021-06-10 12:50:25 +0100

    2.0

---
+++ bepswitch.bat
@@ -0,0 +1,29 @@
+echo off
+:start
+echo =========================
+echo How do you want to play?
+echo =========================
+echo -
+echo 1) Vanilla
+echo 2) Modded (enable Bepinex)
+echo -
+set /p op=Please type the number and press 'return/enter':
+
+if "%op%"=="1" powershell -Command "(gc doorstop_config.ini) -replace 'enabled=true', 'enabled=false' | Out-File -encoding ASCII doorstop_config.ini"
+if "%op%"=="1" goto vanilla
+
+if "%op%"=="2" powershell -Command "(gc doorstop_config.ini) -replace 'enabled=false', 'enabled=true' | Out-File -encoding ASCII doorstop_config.ini"
+if "%op%"=="2" goto modded
+
+:vanilla
+echo =========================
+echo You're now playing Vanilla Valheim!
+goto start
+
+:modded
+echo =========================
+echo You're now playing modded Valheim!
+goto start
+
+:exit
+@exit
