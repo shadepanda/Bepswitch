5c70da55a31c5ea5f44a414cf00e1cfc3c5a0432
lauren-aka-proudunicornmods <proudunicorn.mods@gmail.com>
2021-06-10 23:27:58 +0100

    2.1

--- bepswitch.bat
+++ bepswitch.bat
@@ -15,15 +15,22 @@ if "%op%"=="1" goto vanilla
 if "%op%"=="2" powershell -Command "(gc doorstop_config.ini) -replace 'enabled=false', 'enabled=true' | Out-File -encoding ASCII doorstop_config.ini"
 if "%op%"=="2" goto modded
 
+echo =========================
+echo That doesn't seem right! Please type 1 or 2.
+goto start
+
 :vanilla
 echo =========================
 echo You're now playing Vanilla Valheim!
-goto start
+goto exit
 
 :modded
 echo =========================
 echo You're now playing modded Valheim!
-goto start
+goto exit
 
 :exit
+echo -
+echo This window will now close.
+PAUSE
 @exit
